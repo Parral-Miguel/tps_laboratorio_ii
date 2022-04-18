539043851b4570a012182ebb22f0b45e01382223
Parral-Miguel <60629431+Parral-Miguel@users.noreply.github.com>
2022-04-17 21:34:36 -0300

    Entrega de TP1

---
+++ TP1 Miguel Parral 2°C/Entidades/TP1_Parral Miguel/Calculadora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Operandos;
+
+namespace Calculador
+{
+    public static class Calculadora
+    {
+        // Metodo ValidarOperador valida que el parámetro char de entrada
+        // sea solamente alguno de los designados en el tp. Convierte el char '' en '+'.
+        // Retorna char
+        static private char ValidarOperador(char charOperador)
+        {
+            if (charOperador == '+' || charOperador == '/' || charOperador == '*' || charOperador == '-')
+
+            {
+                return charOperador;
+            }
+
+                return '+';      
+        }
+
+        // Metodo Operar realiza la operaciones de calculadora.
+        // Recibe como parametros de entrada dos Operandos y el char de operación seleccionada.
+        // Retorna double resultado.
+        static public double Operar(Operando n1, Operando n2, char charOperador)
+        {
+            charOperador = ValidarOperador(charOperador);
+            double resultado = 0;
+
+            switch (charOperador)
+            {
+                case '+':
+                    {
+                        resultado = n1 + n2;
+                        break;
+                    }
+
+                case '-':
+                    {
+                        resultado = n1 - n2;
+                        break;
+                    }
+                case '*':
+                    {
+                        resultado = n1 * n2;
+                        break;
+                    }
+                case '/':
+                    {
+                        resultado = n1 / n2;
+                        break;
+                    }
+
+            }
+
+            return resultado; 
+        }
+
+    }
+}
